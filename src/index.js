576a9e33139d82cbfe177c44512d9e8efa57c98f
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-11-04 15:34:41 -0300

    Aula 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 const express = require("express");
+const { v4: uuidv4 } = require('uuid')
 
 const app = express();
+app.use(express.json())
+const customers = [];
+
+/** nosso tipo do cadastro
+ * cpf - string
+ * name - string
+ * id - uuid identificador único universidade
+ * statement - []
+ * 
+ */
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  });
+  return response.status(201).send();
+
+})
 
 app.listen(3333);
