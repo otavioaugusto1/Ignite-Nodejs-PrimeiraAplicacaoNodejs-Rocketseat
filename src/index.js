f26bb00a467ec395b7b628140dddaf5626648b5e
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-11-06 20:03:49 -0300

    aula 8

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const customers = [];
  */
 
   // Middleware
-  function verifyIfExistsAccountCPF(request, response, next){
+function verifyIfExistsAccountCPF(request, response, next){
     const { cpf } = request.headers;
     const customer = customers.find(customer => customer.cpf === cpf);
     if(!customer) {
@@ -23,6 +23,17 @@ const customers = [];
     request.customer = customer;
     return next();
   }
+
+function getBalance(statement){
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type === 'credit'){
+      return acc + operation.amount;
+    }else{
+      return acc - operation.amount;
+    }
+  }, 0)
+  return balance;
+}
 
 
 app.post("/account", (request, response) => {
@@ -61,6 +72,22 @@ app.post("/deposit",verifyIfExistsAccountCPF, (request, response) => {
     type: "credit"
   }
   customer.statement.push(statementOperation);
+  return response.status(201).send();
+})
+
+app.post("/withdraw",verifyIfExistsAccountCPF, (request, response) => {
+  const {amount} = request.body;
+  const {customer} = request;
+  const balance = getBalance(customer.statement)
+  if(balance < amount) {
+    return response.status(400).json({error: 'Saldo insuficiente'})
+  }
+  const statementOperation = {
+    amount: amount,
+    createdAt: new Date(),
+    type: "debit"
+  }
+  customer.statement.push(statementOperation)
   return response.status(201).send();
 })
 
